0cbb2e18aad651f8e9d7bd3187fdecd236f4e3c0
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2021-05-26 22:51:14 +0300

    Remove promise-polyfill package

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,9 +1,6 @@
 import 'tailwindcss/tailwind.css'
 import './style.css'
 
-// Old browser support
-import 'promise-polyfill/src/polyfill';
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
